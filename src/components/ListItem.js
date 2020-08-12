8bd3d8726ba36d6320c59e642e4934c4c08d381a
FayeCarter <fayecarter333@gmail.com>
2020-08-12 19:14:58 +0100

    Add basic styling to List Item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Card, Icon } from "@mrshmllw/smores-react";
 
+import { StyledItem } from '../styles/Item.styles';
+
 function ListItem({ type, data }) {
 
   const {description, flickr_images: images} = data;
@@ -12,10 +14,12 @@ function ListItem({ type, data }) {
 
   return (
     <div >
-      <Card>
-        <h2>{name}</h2>
-        <p>{description}</p>
-        <img src={images[1]} alt="Item" />
+      <Card maxWidth="350px" narrow="true" >
+        <StyledItem>
+          <h2>{name}</h2>
+          <p>{description}</p>
+          <img src={images[1]} alt="Item" />
+        </StyledItem>
         <Icon 
           color="pink6"
           render="arrow"
---
+++ src/styles/Item.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledItem= styled.div`
+
+  h2 {
+    text-align: center;
+  }
+
+  p {
+    text-align: justify;
+  }
+
+  img {
+    max-width: 302px;
+  }
+`;
