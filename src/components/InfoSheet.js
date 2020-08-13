73a71310d1cdc00fb48cbb3be83ac4dff0413a8a
FayeCarter <fayecarter333@gmail.com>
2020-08-13 15:51:51 +0100

    Add basic styling to InfoSheet

--- src/components/InfoSheet.js
+++ src/components/InfoSheet.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { LabelledText } from "@mrshmllw/smores-react";
 
+import { StyledInfoSheet } from '../styles/InfoSheet.styled';
+
 function InfoSheet({ type, data }) {
 
   return (
-    <div className={type}>
+    <StyledInfoSheet className={type}>
       {Object.entries(data).map(([key, value], index) => {
         return (
           <LabelledText
@@ -15,7 +17,7 @@ function InfoSheet({ type, data }) {
           </LabelledText>
         )
       })}
-    </div>
+    </ StyledInfoSheet>
   );
 }
 
---
+++ src/styles/InfoSheet.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledInfoSheet = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  height: 250px;
+  
+  label {
+    padding-top: 15px;
+  }
+
+`;
